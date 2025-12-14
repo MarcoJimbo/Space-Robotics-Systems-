2d444e606d8c5a2dfc5519b894603378e5c550d5
DanieleGallozzi <gallozzi.1950136@studenti.uniroma1.it>
2025-12-14 17:49:07 +0100

    update FK, funzione per jacobiano geomertrico

--- FK.m
+++ FK.m
@@ -1,19 +1,34 @@
-function [T] = FK(rbt,q)
+function [T,p] = FK(rbt,q,varargin)
 % Questa function permette di calcolare la cinematica diretta per un
 % manipolatore con n giunti a partire dal vettore delle coordinate dei
-% giunti.
+% giunti. 
 %
 % INPUT
-% rbt               modello del manipolatore           (file .json)
-% q                 vettore delle variabili dei giunti (vector nx1)
+% rbt                            modello del manipolatore                               (file .json)
+% q                              vettore delle variabili dei giunti                     (vector nx1)
+% varargin{1} = joint_name       nome del giunto di cui si vole calcolare T             (stringa)
 %
 % OUTPUT
-% X                 vettore di coordinate cartesiane   (6x1)
+% T                              matrice di trasformazione joint ---> base              (matrice 4x4)                 
+% p                              vettore posizione del giunto selezionato               (vettore 3x1)
 
 rbt = jsondecode(fileread(rbt));
 n_joints = length(rbt.joints);
+j = rbt.joints;
 T = eye(4);
-for i = 1:n_joints
+
+if isempty(varargin)
+   n = n_joints;
+else
+    joint_name = varargin{1};
+    for i = 1:n_joints
+        if strcmp(j(i).name,joint_name)
+            n = i;
+        end
+    end
+end
+
+for i = 1:n
     j = rbt.joints(i);
     if strcmp(j.type, 'revolute')
        theta = q(i);
@@ -24,3 +39,4 @@ for i = 1:n_joints
     end
     T = T * MDH2tr(theta,d,j.MDH.alpha,j.MDH.a);
 end
+p = T(1:3,4);
---
+++ geomJacobian.m
@@ -0,0 +1,43 @@
+function J = geomJacobian(rbt,varargin)
+% La function calcola la matrice jacobiana del manipolatore. Richiede come
+% INPUT
+% rbt               modello del manipolatore                               (file .json)
+% varargin{1} = q   vettore delle variabili dei giunti                     (vector nx1)
+%
+% OUTPUT
+% J                 matrice jacobiana corrispondente alla configurazione q (matrice 6xn)
+%
+% Nel caso in cui la configurazione non venga fornita, la funzione
+% restituisce la jacobiana come funzione delle variabili dei giunti q.
+
+
+robot = jsondecode(fileread(rbt));
+n_joints = length(robot.joints);
+j = robot.joints;
+Jp = zeros(3,n_joints);
+Jo = zeros(3,n_joints);
+
+% calcolo posizione dell'EE rispetto alla base
+L = robot.end_effector.MDH.a;
+pEE = L * [1;0;0];
+[T,p_last] = FK(rbt,q,j(n_joints).name);
+T = translation2tr(pEE) * T;
+pe = T*p_last;
+
+% jacobian computation
+for i = 2:n_joints+1  
+    [Ti,p] = FK(rbt,q,j(i-1).name);
+    z = Ti(1:3,1:3) * [0;0;1];
+    if strcmp(j(i).type,'revolute')
+       Jp(:,i) = cross(z,(pe-p));
+       Jo(:,i) = z;
+    else
+        Jp(:,i) = z;
+        Jo(:,i) = zeros(3,1);
+    end
+end
+
+J = [Jp(:,2:end); Jo(:,2:end)];
+
+end
+
