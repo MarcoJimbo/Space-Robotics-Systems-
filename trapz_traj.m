1b405bad6d680a6a81f3c5105e06bd4c5864626c
DanieleGallozzi <gallozzi.1950136@studenti.uniroma1.it>
2025-12-16 22:43:24 +0100

    update

--- trapz_traj.m
+++ trapz_traj.m
@@ -1,4 +1,4 @@
-function [theta,dtheta,ddtheta,t_b,t_jk] = trapz_traj(theta,t,k)
+function [theta,dtheta,ddtheta,t_b,t_jk] = trapz_traj(theta,t,ddtheta_max)
 % TRAPZ_TRAJ: questa funzione calcola la traiettoria per andare da una
 % configurazione theta_start a una theta_goal nel tempo t con un profilo di
 % velocità trapezoidale, passando per una serie di configurazioni intermedie 
@@ -7,7 +7,7 @@ function [theta,dtheta,ddtheta,t_b,t_jk] = trapz_traj(theta,t,k)
 % INPUT
 % theta              vettore delle coordinate angolari nel tempo           (vettore Nx1)
 % t                  vettore dei nodi temporali                            (vettore Nx1)
-% k                  fattore di sicurezza su ddtheta (maggiore di 1)       scalare
+% ddtheta_max        massima accelerazione possibile                       (scalare)
 % 
 %
 % OUTPUT
@@ -22,28 +22,25 @@ dt = diff(t);
 dtheta = zeros(N-1,1);
 ddtheta = zeros(N,1);
 t_b = zeros(N,1);
+t_jk = zeros(N-1,1);
 % primo tratto
-ddth =  k * 4 * (theta(2)- theta(1))/dt(1)^2;
-ddtheta(1) = sign(theta(2) - theta(1)) * ddth;
+ddtheta(1) = sign(theta(2) - theta(1)) * ddtheta_max;
 t_b(1) = dt(1) - sqrt(dt(1)^2 - 2 * (theta(2) - theta(1))/ddtheta(1));
 dtheta(1) = (theta(2) - theta(1))/(dt(1) - 0.5*t_b(1));
-
 % punti interni
-for i = 2:N-2
-    dtheta1 = (theta(i+1) - theta(i))/dt(i);
-    dtheta2 = (theta(i+2) - theta(i+1))/dt(i+1);
-    ddth = k * 4 * (theta(i+1) - theta(i))/dt(i)^2;
-    ddtheta(i+1) = sign(dtheta2-dtheta1) * ddth;
-    
-    dtheta(i) = dtheta1;
-    ddtheta(i+1) = sign(dtheta2-dtheta1) * ddth;
-    t_b(i) = (dtheta2 - dtheta1)/ddtheta(i+1);
+for i = 2:N-1
+    dtheta(i) = (theta(i+1) - theta(i))/dt(i);
+    ddtheta(i) = sign(dtheta(i)-dtheta(i-1)) * ddtheta_max;
+    t_b(i) = (dtheta(i) - dtheta(i-1))/ddtheta(i);
 end
 % ultimo tratto
-ddth =  k * 4 * (theta(N) - theta(N-1))/dt(N-1)^2;
-ddtheta(N) = sign(theta(N) - theta(N-1)) * ddth;
+ddtheta(N) = sign(theta(N) - theta(N-1)) * ddtheta_max;
 t_b(N) = dt(N-1) - sqrt(dt(N-1)^2 - 2 * (theta(N) - theta(N-1))/ddtheta(N));
 dtheta(N-1) = (theta(N) - theta(N-1))/(dt(N-1) - 0.5*t_b(N));
 
-t_jk =  dt' - 0.5*t_b(1:N-1,:) - 0.5*t_b(2:N,:);
-
+% calcolo durate tratti lineari
+t_jk(1) = dt(1) - t_b(1) - 0.5 * t_b(2);
+for i = 2:N-2
+    t_jk(i) = dt(i) - 0.5 * t_b(i) - 0.5 * t_b(i+1);
+end
+t_jk(N) = dt(N-1) - t_b(N) - 0.5 * t_b(N-1);
--- whole_trajectory.m
+++ whole_trajectory.m
@@ -13,7 +13,7 @@ tb1_end = tb1_start + t_b(1);
 tb1_span = linspace(tb1_start,tb1_end,n_plot);
 ddq_b1 = ddtheta(1) * ones(1,n_plot);
 dq_b1 = ddq_b1 .* (tb1_span - tb1_start);
-q_b1 = theta(1) + ddtheta(1) * tb1_span.^2 * 0.5;
+q_b1 = theta(1) + ddtheta(1) * (tb1_span - tb1_start).^2 * 0.5;
 % periodo lineare
 tl1_start = tb1_end;
 tl1_end = tl1_start + t_jk(1);
@@ -26,37 +26,35 @@ q = [q q_b1 q_l1]; dq = [dq dq_b1 dq_l1]; ddq = [ddq ddq_b1 ddq_l1];
 t_span = [tb1_span tl1_span];
 
 % punti intermedi
-tl_end = tl1_end;
 for i = 2:N-1
-    q_end = q(end);
     % blending period
-    tb_start = tl_end;
+    tb_start = t_span(end);
     tb_end = tb_start + t_b(i);
     tb_span = linspace(tb_start,tb_end,n_plot);
     ddq_b = ddtheta(i) * ones(1,n_plot);
-    dq_b = ddq_b .* (tb_span - tb_start);
-    q_b = q_end + ddtheta(i) * tb_span.^2 * 0.5;
+    dq_b = dtheta(i-1) + ddq_b .* (tb_span - tb_start);
+    q_b = q(end) + dtheta(i-1) * (tb_span-tb_start) + ddtheta(i) * (tb_span-tb_start).^2 * 0.5;
     % periodo lineare
     tl_start = tb_end;
     tl_end = tl_start + t_jk(i);
     tl_span = linspace(tl_start,tl_end,n_plot);
     ddq_l = zeros(1,n_plot);
     dq_l  = dtheta(i)*ones(1,n_plot);
-    q_l = q_b(end) + dtheta(1) * tl_span;
+    q_l = q_b(end) + dtheta(i) * (tl_span-tl_start);
     
     q = [q q_b q_l]; dq = [dq dq_b dq_l]; ddq = [ddq ddq_b ddq_l];
     t_span = [t_span tb_span tl_span];
 end
 
 % ultimo tratto
-tbN_start = tl_end;
+tbN_start = t_span(end);
 tbN_end = tbN_start + t_b(N);
 tbN_span = linspace(tbN_start,tbN_end,n_plot);
 ddq_bN = ddtheta(N) * ones(1,n_plot);
-dq_bN = ddq_bN .* (tbN_span - tbN_start);
-q_bN = q(end) + ddtheta(N) * tbN_span.^2 * 0.5;
+dq_bN = dtheta(N-1) + ddq_bN .* (tbN_span - tbN_start);
+q_bN = q(end) + dtheta(N-1) * (tbN_span - tbN_start) + ddtheta(N) * (tbN_span - tbN_start).^2 * 0.5;
 
-q = [q q_bN]; dq = [dq dq_bN]; ddq = [ddq ddq_b];
+q = [q q_bN]; dq = [dq dq_bN]; ddq = [ddq ddq_bN];
 t_span = [t_span tbN_span];
 
 if show
