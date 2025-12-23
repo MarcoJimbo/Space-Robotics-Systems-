5374c9cef1ba9a571bf1d7f2358f7bbeb523660d
MarcoJimbo <vittori.1934040@studenti.uniroma1.it>
2025-12-23 17:03:02 +0100

    aggiunto effetto motori
    aggiunto effetto dei motori a simulink (inerzia + attrito)

---
+++ motors_effect.m
@@ -0,0 +1,29 @@
+function[M_m,A_v,A_c] = motors_effect(rbt)
+
+    %%% function che preso in input il modello del braccio robotico
+    %%% restituisce in output la matrice di massa dei motori e la
+    %%% componente di attrito viscoso e di coulomb dovuta ai motori 
+
+    % INPUT:
+    % rbt:                  struct [1x1] = modello manipolatore
+
+    % OUTPPUT:
+    % M_m :                 double [nxn] = matrice di massa motori [kg*m^2]
+    % A_v:                  double [nxn] = matrice dei coefficienti attrito viscoso [N*m*s] 
+    % A_c:                  double [nxn] = vettore degli attriti di Coulomb [N*m]
+
+    % informazioni joint
+    n_joints = rbt.joints_number;
+
+    % inizializzazione matrici
+    M_m = zeros(n_joints,n_joints);
+    A_v = M_m;
+    A_c = zeros(n_joints,1);
+
+    for i = 1:n_joints
+        M_m(i,i) = rbt.joints(i).DC_motor.I * (rbt.joints(i).DC_motor.gear_ratio)^2;
+        A_v(i,i) = (rbt.joints(i).DC_motor.gear_ratio)^2 * rbt.joints(i).DC_motor.b;
+        A_c(i) = rbt.joints(i).DC_motor.tc;
+    end
+
+end
