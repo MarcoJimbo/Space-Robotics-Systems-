73fbd7ee9850d2f5086f497405cd97551c87b264
MarcoJimbo <vittori.1934040@studenti.uniroma1.it>
2025-12-25 16:08:06 +0100

    pre run simulink + encoders
    aggiunto script da far runnare prima di simulink e aggiunto in simulink modellazione degli encoders e sincronia delle frequenze a cui si aggiornano le componenti dinamiche (si devono variare frequenze su script pre run e cambiare le costanti dei guadagni)

---
+++ SIMULINK_pre_run.m
@@ -0,0 +1,35 @@
+%%% MAIN da pre runnare prima di run simulink (salva i parametri necessari a run simulink)
+
+clear; close all; format long; clc
+
+%% IMPORT ROBOT
+
+IDRA = jsondecode(fileread("IDRA.json"));
+
+%% parametri necessari al run
+ 
+% stato iniziale 
+
+ q0 = []; % da prendere da generazione traiettoria
+ qd0 = []; % da prendere da generazione traiettoria
+ qdd0 = []; % da prendere da generazione traiettoria
+
+% frequenze caratteristiche schema di controllo
+
+f_s = 1000; % [Hz] frequenze sensori = aggiornamento nu,mu,errors
+f_u = 100; % [Hz] frequenze aggiornamento M,B,C,G
+
+T_s = 1/f_s;
+T_u = 1/f_u;
+% GAINS
+ki = 1; % da sostituire con  gain scelto
+kp = 1; % da sostituire con  gain scelto
+kd = 1; % da sostituire con  gain scelto
+Ki = eye(IDRA.joints_number) * ki;
+Kp = eye(IDRA.joints_number) * kp;
+Kd = eye(IDRA.joints_number) * kd;
+
+% parametri quantizzazione 
+steps_encoder = 4096;
+DELTA = 2*pi / steps_encoder; % [rad] step angolare quantizzazione encoder
+t_const = 2e-2;
